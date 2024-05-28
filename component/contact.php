5bc3e5dd2f1d56efa4e685fd588e4b6f0223abf3
sadatoki <sadatoki@econosys.jp>
2024-05-28 19:07:40 +0900

    コードの編集

--- component/contact.php
+++ component/contact.php
@@ -1,7 +1,12 @@
 <section class="contact">
     <a href="<?php echo esc_url(get_page_link(9)); ?>" class="contact-inner">
-        <h2 class="contact__title">CONTACT
-            <span class="contact__title--sub">お問い合わせ</span>
+        <h2 class="contact__title1">
+            <span class="text-loop">Feel free to contact.</span>
+            <span class="text-loop">Feel free to contact.</span>
+        </h2>
+        <h2 class="contact__title2">
+            <span class="text-loop">Feel free to contact.</span>
+            <span class="text-loop">Feel free to contact.</span>
         </h2>
     </a>
 </section>
